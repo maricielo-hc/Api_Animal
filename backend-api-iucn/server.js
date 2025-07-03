ce39dde5e28f3497eb36448777e6bb7135d5a393
maricielo-hc <maricielo.mr04@gmail.com>
2025-07-03 16:30:26 -0500

    agregando

---
+++ backend-api-iucn/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+const API_TOKEN = process.env.IUCN_TOKEN;
+
+app.get('/especie/:nombre', async (req, res) => {
+  const nombre = req.params.nombre;
+
+  try {
+    const url = `https://apiv3.iucnredlist.org/api/v3/species/${encodeURIComponent(nombre)}?token=${API_TOKEN}`;
+    const respuesta = await axios.get(url);
+    res.json(respuesta.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "No se pudo obtener la información de la especie." });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
